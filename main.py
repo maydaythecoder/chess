5a02b55638e5acecba4504c5b5a5fa849881ae2f
Muhyadin <Muhyadin.Mohamed.003@gmail.com>
2026-02-09 21:57:54 -0500

    its chess

---
+++ main.py
@@ -0,0 +1,14 @@
+Matrix = [
+    [5,4,3,2,1,3,4,5],
+    [6,6,6,6,6,6,6,6],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [6,6,6,6,6,6,6,6],
+    [5,4,3,2,1,3,4,5]
+]
+
+# board is represented by a matrix
+#pieces are represented by numbers
+#set rules to numbers to represent how the pieces move
