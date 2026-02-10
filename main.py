3e90afc4d80f1aab2755ebf13ff3377cdf150734
Muhyadin <Muhyadin.Mohamed.003@gmail.com>
2026-02-09 22:13:03 -0500

    its still just chess bro

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ Matrix = [
 # board is represented by a matrix
 #pieces are represented by numbers
 #set rules to numbers to represent how the pieces move
+# how do i stop pieces from capturing teammates
+class Pawn:
+    def __init__(self, moves=None, location=None, safe=True):
+        self.moves = moves if moves is not None else []
+        self.location = location if location is not None else [0, 0]
+        self.safe = safe
+
+    def move(self, new_location):
+        if new_location != self.location:
+            self.location = new_location
+            return True
+        return False
