c4cfa682bc2b6a7a7f7ae0d165e445fa7dba6452
Muhyadin <Muhyadin.Mohamed.003@gmail.com>
2026-02-14 20:43:10 -0500

    dude its literally still just chess

--- main.py
+++ main.py
@@ -13,6 +13,55 @@ Matrix = [
 #pieces are represented by numbers
 #set rules to numbers to represent how the pieces move
 # how do i stop pieces from capturing teammates
+
+#PIECES
+'''
+pieces move
+pieces capture
+
+pawn
+def move(target, IsFirst):
+    if IsFirst:
+        moves 2 or 1
+    else
+        moves forward 1
+
+def capture(target, IsFirst, MovedTwo):
+    if piece on frontDiagonal 1 square away
+        able to capture
+    elseif(enemy.IsFirst && MovedTwo):
+        able to capture
+    else unable
+
+bishop
+def move(target):
+    if current position and target position unobstructed
+        move allowed
+    else unable
+
+def capture(target):
+    if current position and target position unobstructed
+        capture allowed
+    else unable
+
+rook
+def move(target):
+    if current position and target position unobstructed
+        move allowed
+    else unable
+
+def capture(target):
+    if current position and target position unobstructed
+        capture allowed
+    else unable
+
+queen
+rook+bishop
+
+king
+rook+bishop capped at 1
+only allow if target location isn't attacked
+'''
 class Pawn:
     def __init__(self, moves=None, location=None, safe=True):
         self.moves = moves if moves is not None else []
